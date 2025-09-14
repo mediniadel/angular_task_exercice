9c073f4b86e0c517068d2af52c6607cf99b7c987
mediniadel <medini.adel@gmail.com>
2025-09-14 02:34:46 +0200

    fix: "app component"

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [
+        App,
+        RouterTestingModule
+      ]
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('devrait créer l\'application', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, task-exercice');
   });
 });
