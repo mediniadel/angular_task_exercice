62725db34f708532bfeffe0c8a6b311c87a5d2d8
mediniadel <medini.adel@gmail.com>
2025-09-14 02:34:31 +0200

    feat: "add routes"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { TaskListComponent } from './features/tasks/components/task-list/task-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: TaskListComponent
+      }
+    ]
+  }
+];
